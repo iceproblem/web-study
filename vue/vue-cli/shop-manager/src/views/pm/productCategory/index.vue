aa455ffe3b78b3bc8b261373cb65b6096b0edea0
iceproblem <1065218471@qq.com>
2020-12-12 16:45:05 +0800

    获取一级分类，实现数据回显“”

--- vue/vue-cli/shop-manager/src/views/pm/productCategory/index.vue
+++ vue/vue-cli/shop-manager/src/views/pm/productCategory/index.vue
@@ -11,36 +11,37 @@
             <el-table
                     :data="listData"
                     border
+                    v-loading="listLoading"
                     style="width: 100%">
                 <el-table-column
-                        prop="id"
                         align="center"
                         label="编号"
                         width="100">
+                        <template slot-scope="scope">{{ scope.$index+1}}</template>
                 </el-table-column>
                 <el-table-column
-                        prop="name"
                         align="center"
                         label="分类名称"
                         width="100">
+                        <template slot-scope="scope">{{ scope.row.name}}</template>
                 </el-table-column>
                 <el-table-column
-                        prop="level"
                         align="center"
                         label="级别"
                         width="100">
+                        <template slot-scope="scope">{{ scope.row.parent_id}}</template>
                 </el-table-column>
                 <el-table-column
-                        prop="productCount"
                         align="center"
                         label="商品数量"
                         width="100">
+                        <template slot-scope="scope">{{ scope.row.product_count}}</template>
                 </el-table-column>
                 <el-table-column
-                        prop="productUnit"
                         align="center"
                         label="数量单位"
                         width="100">
+                        <template slot-scope="scope">{{ scope.row.product_unit}}</template>
                 </el-table-column>
                 <el-table-column
                         align="center"
@@ -48,24 +49,42 @@
                         width="100">
                     <template slot-scope="scope">
                         <el-switch
-                                v-model="scope.row.showStatus"
-                                active-color="1"
-                                inactive-color="0">
+                            @change="handleNavStatusChange(scope.$index,scope.row)"
+                            v-model="scope.row.nav_status"
+                            active-color="1"
+                            inactive-color="0">
                         </el-switch>
                     </template>
                 </el-table-column>
                 <el-table-column
-                        prop="sort"
+                        align="center"
+                        label="是否启用"
+                        width="100">
+                    <template slot-scope="scope">
+                        <el-switch
+                            @change="handleShowStatusChange(scope.$index,scope.row)"
+                            v-model="scope.row.show_status"
+                            active-color="1"
+                            inactive-color="0">
+                        </el-switch>
+                    </template>
+                </el-table-column>
+                <el-table-column
                         align="center"
                         label="排序"
                         width="500">
+                        <template slot-scope="scope">{{ scope.row.sort}}</template>
                 </el-table-column>
                 <el-table-column
                         align="center"
                         label="设置"
                         width="200">
                     <template slot-scope="scope">
-                        <el-button size="mini">查看下一级</el-button>
+                        <el-button 
+                            size="mini"
+                            @click="handleShowNextLevel(scope.$index,scope.row)"
+                            :disabled="scope.row.parent_id | disableNextLevel"
+                        >查看下一级</el-button>
                     </template>
                 </el-table-column>
                 <el-table-column
@@ -73,8 +92,15 @@
                         label="操作"
                         width="285">
                     <template slot-scope="scope">
-                        <el-button size="mini">编辑</el-button>
-                        <el-button size="mini" type="danger">删除</el-button>
+                        <el-button 
+                            size="mini"
+                            @click="handleUpdate(scope.$index,scope.row)"
+                            >编辑</el-button>
+                        <el-button 
+                            size="mini" 
+                            type="danger"
+                            @click="handleDelete(scope.$index,scope.row)"
+                            >删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -95,35 +121,71 @@
 </template>
 
 <script>
+import { getCategory } from '../../../api/categoryApi';
     export default {
         name: "index",
         data(){
             return{
-                listData: [{
-                    id: 1,
-                    name: '安心蔬菜',
-                    level:1,
-                    productCount:100,
-                    productUnit:"斤",
-                    showStatus:1,
-                    sort:1,
-                },{
-                    id: 2,
-                    name: '豆制品',
-                    level:1,
-                    productCount:100,
-                    productUnit:"袋",
-                    showStatus:0,
-                    sort:2,
-                }]
+                listData: null,
+                listLoading:false,
+                parentId:0, //父分类的id
+                pageNum:1,  //  表示当前是第几页
+                pageSize:5, //每页显示多少数据
+                total:0,   //统计分类的数量
             }
         },
+        created() {
+            this.getList(); //在发送ajax请求之前，先把loading变成true
+        },
         methods:{
+            getList(){
+                this.listLoading = true;
+                getCategory(this.parentId,this.pageNum,this.pageSize).then(response=>{
+                // console.log(response)
+                if(response.data.status = 1){
+                    this.listLoading = false;
+                    this.listData = response.data.category_list;
+                    this.total = response.data.category_count;
+                }
+            })
+            },
             handleSizeChange(val) {
                 console.log(`每页 ${val} 条`);
             },
             handleCurrentChange(val) {
                 console.log(`当前页: ${val}`);
+            },
+            // 控制移动端是否显示
+            handleNavStatusChange(){
+
+            },
+            // 控制分类是否启用
+            handleShowStatusChange(){
+
+            },
+            // 查看二级分类
+            handleShowNextLevel(){
+
+            },
+            // 编辑
+            handleUpdate(){
+
+            },
+            // 删除
+            handleDelete(){
+
+            }
+        },
+        filters:{
+            levelFilter(value){
+                if(value === 0){
+                    return "一级"
+                }else{
+                    return "二级"
+                }
+            },
+            disableNextLevel(value){
+                return value !==0
             }
         }
     }
