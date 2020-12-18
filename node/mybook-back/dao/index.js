5a399a2495e379990fbf83ec31722ee65404f0ac
iceproblem <1065218471@qq.com>
2020-12-18 17:51:29 +0800

    缓存清除

--- node/mybook-back/dao/index.js
+++
@@ -1,9 +0,0 @@
-var mysql      = require('mysql');
-var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'myboke'
-});
-//导出db
-module.exports = db
