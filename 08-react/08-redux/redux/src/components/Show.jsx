0d6d278c195469b3d321ee4165b738d02dcd1c7f
iceproblem <1065218471@qq.com>
2021-01-07 20:18:13 +0800

    基本骨架

---
+++ 08-react/08-redux/redux/src/components/Show.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Show extends Component {
+    render() {
+        return (
+            <div>
+                <h1>计数器：0</h1>
+            </div>
+        )
+    }
+}
