7492e425639e7d73fd2f3e488fc2db841e1630fb
iceproblem <1065218471@qq.com>
2021-01-09 09:55:06 +0800

    apiserver端口改为3001

---
+++ 08-react/08-redux/10-todoApiServer/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/api/todos', function(req, res, next) {
+   res.json({
+     status: 200,
+     data: [
+       {id: 1, content: "redux", done: false},
+       {id: 2, content: "react", done: false},
+       {id: 3, content: "react-redux", done: false},
+       {id: 4, content: "umi", done: false}
+     ]
+   })
+});
+
+module.exports = router;
