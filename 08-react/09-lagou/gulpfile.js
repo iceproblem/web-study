7ded74ff3468bbae093b8b5cdfa20d6ef453c1ae
iceproblem <1065218471@qq.com>
2021-01-12 21:38:01 +0800

    glup打包css，js，文件监控，配置开发服务器

--- 08-react/09-lagou/gulpfile.js
+++ 08-react/09-lagou/gulpfile.js
@@ -1,13 +1,53 @@
-let {src,dest,series} = require("gulp");
+let {src,dest,series,watch} = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
 const gulpServer = require('gulp-webserver');
+const webpackStream = require("webpack-stream");
+const path = require("path")
 
 //这是一个任务，将复制html代码到dev
 function copyHTML () {
-    return src("./src/views/**/*.html").pipe(dest("./dev/"))
+    return src("./src/views/**/*.html")
+        .pipe(dest("./dev/"))
+}
+// 编译scss
+function compileSCSS(){
+    return src("./src/scss/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat("all.css"))
+        .pipe(dest('./dev/'));
+}
+// 编译JS
+function complieJS(){
+    return src("./src/js/index.js")
+        .pipe(webpackStream({
+            mode:"development",
+            entry:"./src/js/index.js",
+            output:{
+                path:path.resolve(__dirname,"./dev/"),
+                filename:"all.js"
+            },
+            module:{
+                rules:[
+                    {
+                        test:/\.js$/,
+                        exclude: /node_modules/,
+                        use:{
+                            loader:"babel-loader",
+                            options:{
+                                presets:['@babel/preset-env'],
+                                plugins: ['@babel/plugin-transform-runtime']
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(dest("./dev/"))
 }
 
 // 配置开发服务器
-function startServer(){
+function startServer () {
     return src("./dev/")
         .pipe(gulpServer({
             port:8080,    // 配置端口
@@ -17,6 +57,25 @@ function startServer(){
             directoryListing: false  // 是否显示项目目录结构
         }))
 }
-
+// 文件监控
+function watchFile(){
+    watch("./src/scss/**/*.scss",(cb)=>{
+        compileSCSS();
+        cb()
+    })
+    watch("./src/views/**/*.thml",(cb)=>{
+        copyHTML();
+        cb()
+    })
+    watch("./src/js/index.js",(cb)=>{
+        complieJS();
+        cb()
+    })
+}
+// 把libs下的文件，copy到dev下面
+function copyLibs(){
+    return src("./src/libs/**/*.*")
+        .pipe(dest("./dev/libs/"))
+}
 // 导出任务
-exports.default = series(copyHTML,startServer)
+exports.default = series(copyHTML,compileSCSS,complieJS,copyLibs,startServer,watchFile)
---
+++ 08-react/09-lagou/src/js/index.js
@@ -0,0 +1,19 @@
+// alert("你好~人类")
+// class App{
+//     constructor(){
+//         console.log("constructor");
+//         this.init();
+//     }
+//     async init(){
+//         let res = await this.getName()
+//         alert(res);
+//     }
+//     getName(){
+//         return new Promise((resolve,reject)=>{
+//             setTimeout(()=>{
+//                 resolve("包包")
+//             },3000)
+//         })
+//     }
+// }
+// new App();
